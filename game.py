00d80f624363e841a1afb77c7b34a9170cf115c6
Jamilli <jamillileal8@gmail.com>
2025-03-05 16:39:19 -0300

    second commit

--- game.py
+++ game.py
@@ -46,42 +46,43 @@ class Player(pygame.sprite.Sprite):
 
 
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, type):
+    def __init__(self, type,):
         super().__init__()
 
-        if type == 'fly':
-            fly_1 = pygame.image.load('asset/bat1.png').convert_alpha()
-            fly_2 = pygame.image.load('asset/bat2.png').convert_alpha()
-            self.frames = [fly_1, fly_2]
-            y_pos = 210
+        if type == 'bat':
+            bat_1 = pygame.image.load('asset/bat1.png').convert_alpha()
+            bat_2 = pygame.image.load('asset/bat2.png').convert_alpha()
+            self.frames = [bat_1, bat_2]
+            y_pos = 170
         else:
-            snail_1 = pygame.image.load('asset/frog1.png').convert_alpha()
-            snail_2 = pygame.image.load('asset/frog2.png').convert_alpha()
-            self.frames = [snail_1, snail_2]
+            frog_1 = pygame.image.load('asset/frog1.png').convert_alpha()
+            frog_2 = pygame.image.load('asset/frog2.png').convert_alpha()
+            self.frames = [frog_1, frog_2]
             y_pos = 300
+
 
         self.animation_index = 0
         self.image = self.frames[self.animation_index]
         self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
 
-    def animation_state(self):
+    def animation(self):
         self.animation_index += 0.1
         if self.animation_index >= len(self.frames): self.animation_index = 0
         self.image = self.frames[int(self.animation_index)]
 
     def update(self):
-        self.animation_state()
+        self.animation()
         self.rect.x -= 6
         self.destroy()
 
     def destroy(self):
-        if self.rect.x <= -100:
+        if self.rect.x <= -50: #100
             self.kill()
 
 
 def display_score():
-    current_time = int(pygame.time.get_ticks() / 1000) - start_time
-    score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
+    current_time = int(pygame.time.get_ticks() / 1000) - startTime
+    score_surf = font.render(f'Pontuacao: {current_time}', False, (86, 7, 12))
     score_rect = score_surf.get_rect(center=(400, 50))
     screen.blit(score_surf, score_rect)
     return current_time
@@ -97,14 +98,14 @@ def collision_sprite():
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Runner')
+pygame.display.set_caption('Meu Jogo')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('Pixeltype.ttf', 50)
-game_active = False
-start_time = 0
+font = pygame.font.Font('Pixeltype.ttf', 50)
+game_ativo = False
+startTime = 0
 score = 0
-bg_music = pygame.mixer.Sound('asset/music.wav')
-bg_music.play(loops=-1)
+music = pygame.mixer.Sound('asset/music.wav')
+music.play(loops=-1)
 
 # Groups
 player = pygame.sprite.GroupSingle()
@@ -112,23 +113,23 @@ player.add(Player())
 
 obstacle_group = pygame.sprite.Group()
 
-sky_surface = pygame.image.load('asset/bg_castle.png').convert()
-ground_surface = pygame.image.load('asset/ground.png').convert()
+bg_castle = pygame.image.load('asset/bg_castle.png').convert()
+ground = pygame.image.load('asset/ground.png').convert()
 
 # Intro screen
-player_stand = pygame.image.load('asset/playerstand.png').convert_alpha()
-player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
-player_stand_rect = player_stand.get_rect(center=(400, 200))
+player_img = pygame.image.load('asset/playerstand.png').convert_alpha()
+player_img = pygame.transform.rotozoom(player_img, 0, 2)
+player_img_rect = player_img.get_rect(center=(400, 200))
 
-game_name = test_font.render('Pixel Runner', False, (111, 196, 169))
+game_name = font.render('Corrida', False, (86, 7, 12))
 game_name_rect = game_name.get_rect(center=(400, 80))
 
-game_message = test_font.render('Press space to run', False, (111, 196, 169))
-game_message_rect = game_message.get_rect(center=(400, 330))
+start_message = font.render('Pressione  SPACE  para jogar', False, (86, 7, 12))
+start_message_rect = start_message.get_rect(center=(400, 330))
 
 # Timer
-obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer, 1500)
+obstacleTimer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacleTimer, 1500)
 
 while True:
     for event in pygame.event.get():
@@ -136,18 +137,18 @@ while True:
             pygame.quit()
             exit()
 
-        if game_active:
-            if event.type == obstacle_timer:
-                obstacle_group.add(Obstacle(choice(['fly', 'snail', 'snail', 'snail'])))
+        if game_ativo:
+            if event.type == obstacleTimer:
+                obstacle_group.add(Obstacle(choice(['bat', 'frog', 'frog', 'frog'])))
 
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                game_active = True
-                start_time = int(pygame.time.get_ticks() / 1000)
+                game_ativo = True
+                startTime = int(pygame.time.get_ticks() / 1000)
 
-    if game_active:
-        screen.blit(sky_surface, (0, 0))
-        screen.blit(ground_surface, (0, 300))
+    if game_ativo:
+        screen.blit(bg_castle, (0, 0))
+        screen.blit(ground, (0, 300))
         score = display_score()
 
         player.draw(screen)
@@ -156,18 +157,18 @@ while True:
         obstacle_group.draw(screen)
         obstacle_group.update()
 
-        game_active = collision_sprite()
+        game_ativo = collision_sprite()
 
     else:
         screen.fill((94, 129, 162))
-        screen.blit(player_stand, player_stand_rect)
+        screen.blit(player_img, player_img_rect)
 
-        score_message = test_font.render(f'Your score: {score}', False, (111, 196, 169))
+        score_message = font.render(f'Sua pontuacao: {score}', False, (86, 7, 12))
         score_message_rect = score_message.get_rect(center=(400, 330))
         screen.blit(game_name, game_name_rect)
 
         if score == 0:
-            screen.blit(game_message, game_message_rect)
+            screen.blit(start_message, start_message_rect)
         else:
             screen.blit(score_message, score_message_rect)
 
